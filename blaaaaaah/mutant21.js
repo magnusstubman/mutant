955b005b439fac8a0e3be55b657b5fb3bbdffeb6
Magnus Stubman <magnus@stubman.eu>
2014-02-17 22:08:58 +0100

    add example mutants

---
+++ blaaaaaah/mutant21.js
@@ -0,0 +1,9 @@
+var add = function (a, b) {
+    var p = 10;
+    if (p < 5 && 5 != 4 && 5 <= 4) {
+        if (1 < 2) {
+            console.log('blah');
+        }
+        return a + b;
+    }
+};
